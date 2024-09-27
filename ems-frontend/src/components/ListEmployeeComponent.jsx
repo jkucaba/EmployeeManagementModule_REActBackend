240e60f472727e2d7a71f33b628527e2df00c23f
Jakub <jakub.kucaba@gmail.com>
2024-09-27 17:46:32 +0200

    ListEmployeeComponent

---
+++ ems-frontend/src/components/ListEmployeeComponent.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+
+const ListEmployeeComponent = () => {
+
+const dummyData = [
+    {
+      "id": 1,
+      "firstName": 'Jakub',
+      "lastName": 'Kubica',
+      "email": 'jkubica@gmail.com'
+    },
+    {
+        "id": 2,
+        "firstName": 'Łukuś',
+        "lastName": 'Samotny',
+        "email": 'lsamotny@gmail.com'
+    },
+    {
+        "id": 3,
+        "firstName": 'Patryk',
+        "lastName": 'Evans',
+        "email": 'pevans@gmail.com'
+    }
+]
+
+  return (
+    <div className='container'>
+
+        <h2 className='text-center'>List of Employees</h2>
+        <table className='table table-striped table-bordered'>
+            <thead>
+                <tr>
+                    <th>Employee Id</th>
+                    <th>Employee First Name</th>
+                    <th>Employee Last Name</th>
+                    <th>Employee Email</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    dummyData.map(employee => 
+                        <tr key={employee.id}>
+                            <td>{employee.id}</td>
+                            <td>{employee.firstName}</td>
+                            <td>{employee.lastName}</td>
+                            <td>{employee.email}</td>
+                        </tr>
+                    )
+                }
+            </tbody>
+        </table>
+    </div>
+  )
+}
+
+export default ListEmployeeComponent
